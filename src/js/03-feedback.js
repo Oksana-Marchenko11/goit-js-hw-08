57338d26e596e29184a688192b0651aa76236ec4
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-16 14:52:01 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+var throttle = require('lodash.throttle');
 
+const myForm = document.querySelector(".feedback-form");
+// console.log(myForm)
+let elem = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+console.log(elem);
+if ('email' in elem) {
+    myForm.elements.email.value = elem.email;
+}
+if ('message' in elem) {
+    myForm.elements.message.value = elem.message;
+}
+
+myForm.addEventListener('input', throttle(function (e) {
+    // console.log(e.currentTarget.elements)
+    const {
+        elements: { email, message }
+    } = e.currentTarget;
+
+    elem[email.name] = email.value;
+    elem[message.name] = message.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(elem))
+}, 500));
+myForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.removeItem('feedback-form-state');
+    myForm.reset();
+
+});
