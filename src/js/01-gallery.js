b48d8192b28bd413b1a1687b63520931155c8e6d
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-16 12:42:49 +0300

    first+second done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const makeList = galleryItems.map((item) =>
+  `<li class="gallery__item">
+      <a class="gallery__link" href= ${item.original}>
+        <img class="gallery__image" src=${item.preview} alt=${item.description}>
+      </a>
+    </li>`
+).join("");
+
+document.querySelector(".gallery").innerHTML = makeList;
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+
+
+
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+let currentTime = localStorage.getItem("videoplayer-current-time");
+currentTime ||= 0;
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(currentTime);
