bbb3bcd43e45eb1e33eac9aa718939ffa770b208
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-17 16:18:25 +0300

    fixed description(alt)=>full description

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,8 +6,8 @@ import { galleryItems } from './gallery-items';
 
 const makeList = galleryItems.map((item) =>
   `<li class="gallery__item">
-      <a class="gallery__link" href= ${item.original}>
-        <img class="gallery__image" src=${item.preview} alt=${item.description}>
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}">
       </a>
     </li>`
 ).join("");
